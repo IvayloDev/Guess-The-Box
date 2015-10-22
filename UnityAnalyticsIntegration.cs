1fe17091b35104aaf02a3b32ac51302dd9555a1b
Ivaylo Palchev <ivopalchev@gmail.com>
2015-10-22 12:29:08 +0300

    Update UnityAnalyticsIntegration.cs

--- UnityAnalyticsIntegration.cs
+++ UnityAnalyticsIntegration.cs
@@ -6,7 +6,7 @@ public class UnityAnalyticsIntegration : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		const string projectId = "e0688fa5-f695-41f0-9c14-a6fbe30cb763";
+		const string projectId = "PROJECT_ID";
 		UnityAnalytics.StartSDK (projectId);
 	}
 }
